ae5c908fb37017325e53eda4d7ec72f7d556669d
tolimacner <142923729+tolimacner@users.noreply.github.com>
2024-09-22 09:27:02 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,4 +32,4 @@ def weather():
         return render_template('index.html', error=error_message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
