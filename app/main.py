6978527b3aae733e98af2f514771f23a18b6bd4a
Toli <tmacner@itset.co.il>
2025-08-13 15:30:19 +0300

    Set up CI/CD workflows and configs

--- app/main.py
+++ app/main.py
@@ -34,5 +34,4 @@ def weather():
         return render_template('index.html', error=error_message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
